967fe4db57e4ff50980a24516c3bbec60542a583
matos17igor <igorm17_pvp@hotmail.com>
2025-10-30 15:05:08 -0300

    cria paginas de dashbord e postagem para usuarios cadastrados

--- miniblog/src/components/Navbar.jsx
+++ miniblog/src/components/Navbar.jsx
@@ -7,6 +7,8 @@ import { useAuthValue } from "../context/AuthContext";
 import styles from "./Navbar.module.css";
 
 const Navbar = () => {
+  const { user } = useAuthValue();
+
   return (
     <nav className={styles.navbar}>
       <NavLink to="/" className={styles.brand}>
@@ -21,22 +23,46 @@ const Navbar = () => {
             Inicio
           </NavLink>
         </li>
-        <li>
-          <NavLink
-            to="/login"
-            className={({ isActive }) => (isActive ? styles.active : " ")}
-          >
-            Entrar
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="/register"
-            className={({ isActive }) => (isActive ? styles.active : " ")}
-          >
-            Cadastrar
-          </NavLink>
-        </li>
+        {!user && (
+          <>
+            <li>
+              <NavLink
+                to="/login"
+                className={({ isActive }) => (isActive ? styles.active : " ")}
+              >
+                Entrar
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/register"
+                className={({ isActive }) => (isActive ? styles.active : " ")}
+              >
+                Cadastrar
+              </NavLink>
+            </li>
+          </>
+        )}
+        {user && (
+          <>
+            <li>
+              <NavLink
+                to="/posts/create"
+                className={({ isActive }) => (isActive ? styles.active : " ")}
+              >
+                Novo post
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/dashbord"
+                className={({ isActive }) => (isActive ? styles.active : " ")}
+              >
+                Dashbord
+              </NavLink>
+            </li>
+          </>
+        )}
         <li>
           <NavLink
             to="/about"
---
+++ miniblog/src/pages/Dashbord/Dashbord.jsx
@@ -0,0 +1,11 @@
+import style from "./Dashbord.module.css";
+
+const Dashbord = () => {
+  return (
+    <div>
+      <h1>Dashbord</h1>
+    </div>
+  );
+};
+
+export default Dashbord;
